dde2ede65b6bbab6fa62ba4b70b4f27170b9db76
discuspro <discuspro@yahoo.com>
2017-06-26 22:06:01 -0500

    update

--- ITCL_Sequest_compiler/ITCL_Sequest_compiler.py
+++ ITCL_Sequest_compiler/ITCL_Sequest_compiler.py
@@ -1,1 +1,30 @@
+import xlrd
 
+path = "C:\Users\Dustin Holden\Source\Repos\python\Py_project\ITCL_Sequest_compiler\testbook1.xlsx"
+
+def open_file(path):
+    """
+    Open and read an Excel file
+    """
+    book = xlrd.open_workbook(path)
+    # print number of sheets
+    print book.nsheets
+    # print sheet names
+    print book.sheet_names()
+    # get the first worksheet
+    first_sheet = book.sheet_by_index(0)
+    # read a row
+    print first_sheet.row_values(0)
+    # read a cell
+    cell = first_sheet.cell(0,0)
+    print cell
+    print cell.value
+    # read a row slice
+    print first_sheet.row_slice(rowx=0,
+                                start_colx=0,
+                                end_colx=2)
+
+
+def main():
+    #saver
+main
