2bcc1ac6b5a40df675bd7e7ed1799d593740fe06
discuspro <discuspro@yahoo.com>
2017-06-26 23:17:05 -0500

    update

--- ITCL_Sequest_compiler/ITCL_Sequest_compiler.py
+++ ITCL_Sequest_compiler/ITCL_Sequest_compiler.py
@@ -1,8 +1,15 @@
 import xlrd
 
-path = r'C:\testbook1.xlsx'
+path = "testbook1.xlsx"
 
 book = xlrd.open_workbook(path)
 sheet = book.sheet_by_index(0)
 a1 = sheet.cell_value(rowx=2, colx=4)
 print (a1)
+
+import xlwt
+ 
+book = xlwt.Workbook()
+sheet = book.add_sheet("PySheet1")
+sheet.write(0, 0, a1) # row, column, value
+book.save("test1.xls")
